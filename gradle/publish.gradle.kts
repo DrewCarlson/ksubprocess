bdf39d700aca5a2e8a144b1ccaf0b1d172ecadbd
Drew Carlson <awcarlson@protonmail.com>
2022-03-15 10:35:22 -0700

    Publish to explicit staging repository

--- gradle/publish.gradle.kts
+++ gradle/publish.gradle.kts
@@ -1,3 +1,6 @@
+import java.net.URL
+import java.util.Base64
+
 apply(plugin = "maven-publish")
 apply(plugin = "signing")
 apply(plugin = "org.jetbrains.dokka")
@@ -8,12 +11,15 @@ System.getenv("GITHUB_REF")?.let { ref ->
     }
 }
 
+val isSnapshot by lazy { version.toString().endsWith("SNAPSHOT") }
+
 val mavenUrl: String by extra
 val mavenSnapshotUrl: String by extra
 val signingKey: String? by project
 val signingPassword: String? by project
 val sonatypeUsername: String? by project
 val sonatypePassword: String? by project
+val sonatypeStagingProfile: String? by project
 val pomProjectUrl: String by project
 val pomProjectDescription: String by project
 val pomScmUrl: String by project
@@ -25,6 +31,28 @@ val pomLicenseDistribution: String by project
 
 task<Jar>("javadocJar") {
     archiveClassifier.set("javadoc")
+}
+
+val repositoryId by lazy {
+    val hasPublishingTask = gradle.startParameter.taskNames.any {
+        it == "publish" || (it.startsWith("publish") && it.contains("MavenRepository"))
+    }
+    if (!hasPublishingTask || isSnapshot) return@lazy ""
+    if (!rootProject.extra.has("publishRepositoryId")) {
+        val auth = Base64.getEncoder().encode("$sonatypeUsername:$sonatypePassword".toByteArray())
+        val body = "<promoteRequest><data><description>Repository for ${version}</description></data></promoteRequest>"
+        val id = URL("${mavenUrl}profiles/$sonatypeStagingProfile/start").openConnection().run {
+            doOutput = true
+            setRequestProperty("Authorization", "Basic ${auth.decodeToString()}")
+            setRequestProperty("Content-Type", "application/xml")
+            getOutputStream().write(body.toByteArray())
+            connect()
+            val response = getInputStream().readBytes().decodeToString()
+            response.substringAfter("<stagedRepositoryId>").substringBefore("</stagedRepositoryId>")
+        }
+        rootProject.extra.set("publishRepositoryId", id)
+    }
+    rootProject.extra.get("publishRepositoryId")
 }
 
 configure<PublishingExtension> {
@@ -65,10 +93,10 @@ configure<PublishingExtension> {
     }
     repositories {
         maven {
-            url = if (version.toString().endsWith("SNAPSHOT")) {
+            url = if (isSnapshot) {
                 uri(mavenSnapshotUrl)
             } else {
-                uri(mavenUrl)
+                uri("${mavenUrl}deployByRepositoryId/$repositoryId")
             }
             credentials {
                 username = sonatypeUsername
@@ -79,7 +107,7 @@ configure<PublishingExtension> {
 }
 
 configure<SigningExtension> {
-    isRequired = !version.toString().endsWith("SNAPSHOT")
+    isRequired = !isSnapshot
     useInMemoryPgpKeys(signingKey, signingPassword)
     sign((extensions["publishing"] as PublishingExtension).publications)
 }
